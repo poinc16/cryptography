b298754044a46fd03003396469d9f2b8a6f41829
poinc16 <matheus.barrosc@gmail.com>
2022-03-04 15:47:19 -0300

    Ajustes de espaçamento

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ initial_choice = func.main_crypt()
 
 if initial_choice == 'cfile':
     pass
+
 elif initial_choice == 'dfile':
     pass
+
 elif initial_choice == 'ctext':
     text_to_crypt = input('Digite o texto a ser criptografado: ')
     crypted_text = func.text_crypt(text_to_crypt)
     print(func.make_file_crypt(crypted_text))
+
 elif initial_choice == 'dtext':
     text_to_decrypt = input('Digite o texto a ser descriptografado: ')
     decrypted_text = func.text_decrypt(text_to_decrypt)
